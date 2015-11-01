dd0ad23e5278bb5ff34f84faa6c38e11a6123b59
Kyle Lexmond <github@kyl191.net>
2015-11-01 15:25:52 -0500

    Trying to work around Dropbox's frequent 504 errors

--- borealis.py
+++ borealis.py
@@ -55,35 +55,43 @@ class Borealis:
     Also optionally deletes everything that is downloaded
     Doesn't attempt to do error checking
     """
-    files = self.client.metadata(remote_dir)["contents"]
+    while True:
+      try:
+        files = self.client.metadata(remote_dir)["contents"]
+        break
+      except dropbox.rest.ErrorResponse as e:
+        print(e)
     for f in files:
       path = f.get("path")
       print("Considering %s" % f.get("path"))
       # If the file entry is a directory, recurse into it
-      if f.get("is_dir"):
-        get_files_in_folder(path, remove_base)
-      else:
-        # Dropbox path might have a base component we want to remove
-        if remove_base:
-          path = path.partition(remove_base)[2]
-        # Get rid of the leading slash if present
-        if path.startswith("/"):
-          path = path[1:]
-        # Build local destination path
-        path = os.path.join(dest_dir, path)
-        # Split the directory and filename
-        directory, _ = os.path.split(path)
-        # If the eventual destination directory doesn't exist, create it
-        if not os.path.isdir(directory):
-          os.makedirs(directory, 0755)
-        # Download the file
-        # Because we've manipulated path, go back to using f.get("path") for the dropbox access
-        with open(path, 'wb') as dest, self.client.get_file(f.get("path")) as src:
-          dest.write(src.read())
-      # Regardless of whether it was a file or folder, delete it from Dropbox
-      if self.config.get("delete_remote"):
-        print("Deleting %s" % f.get("path"))
-        self.client.file_delete(f.get("path"))
+      try:
+        if f.get("is_dir"):
+          get_files_in_folder(path, remove_base)
+        else:
+          # Dropbox path might have a base component we want to remove
+          if remove_base:
+            path = path.partition(remove_base)[2]
+          # Get rid of the leading slash if present
+          if path.startswith("/"):
+            path = path[1:]
+          # Build local destination path
+          path = os.path.join(dest_dir, path)
+          # Split the directory and filename
+          directory, _ = os.path.split(path)
+          # If the eventual destination directory doesn't exist, create it
+          if not os.path.isdir(directory):
+            os.makedirs(directory, 0755)
+          # Download the file
+          # Because we've manipulated path, go back to using f.get("path") for the dropbox access
+          with open(path, 'wb') as dest, self.client.get_file(f.get("path")) as src:
+            dest.write(src.read())
+        # Regardless of whether it was a file or folder, delete it from Dropbox
+        if self.config.get("delete_remote"):
+          print("Deleting %s" % f.get("path"))
+          self.client.file_delete(f.get("path"))
+      except dropbox.rest.ErrorResponse as e:
+        print(e)
 
   def wait_for_activity(self, folder="/"):
     """
